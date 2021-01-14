92f8afd736d60c61c4791b9f551ce7cb51558418
brian8234214 <63779101+brian8234214@users.noreply.github.com>
2021-01-14 22:18:21 +0800

    Update 10.1delete.php

--- 10.1delete.php
+++ 10.1delete.php
@@ -1,7 +1,9 @@
 <?php
     error_reporting();
+//error_reporting()函式是用來設定錯誤級別並返回當前級別的。
     $conn=mysqli_connect("localhost","root","","mydb");
     // delete from bulletin where bid=???
+////連接mysql數據庫中的mydb資料庫
     $sql="delete from bulletin where bid={$_GET[bid]}";
     //echo $sql;
     if (!mysqli_query($conn, $sql))
