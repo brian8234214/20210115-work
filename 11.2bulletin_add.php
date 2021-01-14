7be0f3bde27560382b01b7dbca796f9d88a2a128
brian8234214 <63779101+brian8234214@users.noreply.github.com>
2021-01-14 22:23:21 +0800

    Update 11.2bulletin_add.php

--- 11.2bulletin_add.php
+++ 11.2bulletin_add.php
@@ -1,8 +1,12 @@
 ﻿<?php
     session_start();
+// //啟動Session的使用
 if (!isset($_SESSION['id'])){   
+    ////檢查Session值是否存在
     echo "請登入系統";
     echo "<meta http-equiv='refresh' content='3;url=index.html''>"; 
+    //meta http-equiv='refresh' content='3頁面定期重新整理，content後面跟的 是時間（單位秒）
+    //url=index.html如果加url的，則會重新定向到指定的網頁
 }else{
     $conn = mysqli_connect("localhost", "root", "", "mydb");
     if (mysqli_connect_error($conn))
